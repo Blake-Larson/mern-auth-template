833eecc70dcdf45752a2ad649fc2509298a738e3
Blake Larson <Blarson224@gmail.com>
2022-09-15 14:03:08 -0700

    removed old dependencies no longer in use

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
-const flash = require('express-flash');
 const logger = require('morgan');
 const connectDB = require('./config/database');
 const mainRoutes = require('./routes/main');
@@ -41,8 +40,6 @@ app.use(
 // Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
-
-app.use(flash());
 
 app.use('/', mainRoutes);
 app.use('/todos', todoRoutes);
